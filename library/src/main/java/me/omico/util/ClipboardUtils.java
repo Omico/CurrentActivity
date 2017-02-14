3120debe0c8ce945c744cf6eed93b8629385f029
Omico <294958213@qq.com>
2017-02-14 14:49:23 +0800

    Add about page
    Former-commit-id: 443d8c42cbda528fc76745a50b8e6e5a0d363ee6

--- library/src/main/java/me/omico/util/ClipboardUtils.java
+++ library/src/main/java/me/omico/util/ClipboardUtils.java
@@ -3,6 +3,7 @@ package me.omico.util;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.support.annotation.StringRes;
 import android.widget.Toast;
 
 import me.omico.R;
@@ -12,8 +13,26 @@ import me.omico.R;
  */
 
 public class ClipboardUtils {
+
+    public static void copyToClipboard(Context context, @StringRes int resId) {
+        copyToClipboard(context, context.getPackageName(), context.getString(resId));
+    }
+
     public static void copyToClipboard(Context context, String text) {
         copyToClipboard(context, context.getPackageName(), text);
+    }
+
+
+    public static void copyToClipboard(Context context, @StringRes int resId, String text) {
+        copyToClipboard(context, context.getString(resId), text);
+    }
+
+    public static void copyToClipboard(Context context, String name, @StringRes int resId) {
+        copyToClipboard(context, name, context.getString(resId));
+    }
+
+    public static void copyToClipboard(Context context, @StringRes int resIdName, @StringRes int resIdText) {
+        copyToClipboard(context, context.getString(resIdName), context.getString(resIdText));
     }
 
     public static void copyToClipboard(Context context, String name, String text) {
