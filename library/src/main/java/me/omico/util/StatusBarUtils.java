2142ca8548ecb9abe61c4a892c81dcf8f1d0b895
Omico <294958213@qq.com>
2017-02-18 14:13:51 +0800

    Fix: Statusbar elevation
    Former-commit-id: a2b7d000c42fa0f0032616ac0284ba14877a52f4

---
+++ library/src/main/java/me/omico/util/StatusBarUtils.java
@@ -0,0 +1,31 @@
+package me.omico.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Omico on 2/16/2017.
+ */
+
+public class StatusBarUtils {
+    public static void addStatusBarView(Activity activity, int statusBarColor) {
+        View statusBarView = new View(activity);
+        statusBarView.setBackgroundColor(ContextCompat.getColor(activity, statusBarColor));
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                getStatusBarHeight(activity));
+        ((ViewGroup) activity.findViewById(android.R.id.content)).addView(statusBarView, layoutParams);
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+}
