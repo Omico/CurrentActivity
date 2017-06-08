113e77aa537af1ad5d3ee17d7cb37f5cd414d549
Omico <294958213@qq.com>
2017-06-08 19:05:06 +0800

    Add custom click & long press
    Former-commit-id: 29bc1358b5db372649673bd75c038c658337ba58

--- app/src/main/java/me/omico/currentactivity/Constants.java
+++ app/src/main/java/me/omico/currentactivity/Constants.java
@@ -10,11 +10,15 @@ public final class Constants {
 
     public static final String ENABLE_FLOAT_WINDOW = "enable_float_window";
     public static final String BOOT_COMPLETED = "boot_completed";
+    public static final String GESTURE_CLICK = "gesture_click";
+    public static final String GESTURE_LONG_PRESS = "gesture_long_press";
     public static final String ABOUT = "about";
 
     public static final String IS_FIRST_OPEN = "is_first_open";
     public static final String IS_SHORTCUT_OPEN = "is_shortcut_open";
 
+    public static final String ACTION_GESTURE_COPY = "me.omico.currentactivity.action.gesture_copy";
+    public static final String ACTION_GESTURE_HIDE = "me.omico.currentactivity.action.gesture_hide";
     public static final String ACTION_QUICK_START = "me.omico.currentactivity.action.quick_start";
     public static final String ACTION_STOP = "me.omico.currentactivity.action.stop";
 }
