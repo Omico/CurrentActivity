0823e103a0ec3ee2faaf867eaea21e07b34c33a9
Omico <294958213@qq.com>
2017-07-23 11:27:18 +0800

    Optimize ZUI
    Former-commit-id: 0b4dc5e675220933f8a42f7d41fcfdf064e4c74c

---
+++ library/src/main/java/me/omico/util/device/CheckOSVariant.java
@@ -0,0 +1,39 @@
+package me.omico.util.device;
+
+import java.util.Objects;
+
+import static me.omico.util.device.Property.getProperty;
+
+/**
+ * @author Omico 2017/7/22
+ */
+
+public class CheckOSVariant {
+
+    public static final String COMMON = "common";
+    public static final String UNKNOWN = "unknown";
+    public static final String ZUI = "zui";
+
+    public static final String PROP_ZUI_VERSION = "ro.com.zui.version";
+
+    private static String variant;
+
+    public CheckOSVariant init() {
+        variant = COMMON;
+        if (isZui()) variant = ZUI;
+        return this;
+    }
+
+    public String getOSVariant() {
+        return variant;
+    }
+
+    public static boolean isZui() {
+        return checkProp(PROP_ZUI_VERSION);
+    }
+
+    private static boolean checkProp(String prop) {
+        String value = getProperty(prop);
+        return value != null && !Objects.equals(value, UNKNOWN) && !value.isEmpty();
+    }
+}
---
+++ library/src/main/java/me/omico/util/device/Property.java
@@ -0,0 +1,46 @@
+package me.omico.util.device;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Omico 2017/7/23
+ */
+
+public class Property {
+
+    private static final String SYSTEM_PROPERTIES = "android.os.SystemProperties";
+    private static final String GET = "get";
+    private static final String SET = "set";
+    private static final String UNKNOWN = "unknown";
+
+    private static Class<?> c;
+
+    static {
+        try {
+            c = Class.forName(SYSTEM_PROPERTIES);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        String value;
+        try {
+            Method get = c.getMethod(GET, String.class, String.class);
+            value = (String) (get.invoke(c, key, UNKNOWN));
+        } catch (Exception e) {
+            e.printStackTrace();
+            value = null;
+        }
+        return value;
+    }
+
+    public static void setProperty(String key, String value) {
+        try {
+            Method set = c.getMethod(SET, String.class, String.class);
+            set.invoke(c, key, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
