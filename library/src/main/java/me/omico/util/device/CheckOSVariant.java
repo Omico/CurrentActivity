62300da95b375da0ddeea8c39cc177e1485569d2
Omico <294958213@qq.com>
2017-08-21 17:01:24 +0800

    Optimize Flyme
    Former-commit-id: 295ead8203c90a02fea4576b7bc3e7db31d8e1a7

--- library/src/main/java/me/omico/util/device/CheckOSVariant.java
+++ library/src/main/java/me/omico/util/device/CheckOSVariant.java
@@ -10,14 +10,21 @@ public class CheckOSVariant extends Property {
 
     public static final String COMMON = "common";
     public static final String ZUI = "zui";
+    public static final String FLYME = "flyme";
 
+    public static final String PROP_COMMON_BUILD_DISPLAY_ID = "ro.build.display.id";
     public static final String PROP_ZUI_VERSION = "ro.com.zui.version";
+    public static final String PROP_FLYME_MODEL = "ro.product.flyme.model";
 
     private static String variant;
 
     public CheckOSVariant init() {
         variant = COMMON;
-        if (isZui()) variant = ZUI;
+        if (isZui()) {
+            variant = ZUI;
+        } else if (isFlyme()) {
+            variant = FLYME;
+        }
         return this;
     }
 
@@ -26,11 +33,23 @@ public class CheckOSVariant extends Property {
     }
 
     public static boolean isZui() {
-        return checkProp(PROP_ZUI_VERSION);
+        return checkPropExist(PROP_ZUI_VERSION);
+    }
+
+    public static boolean isFlyme() {
+        return checkPropExist(PROP_FLYME_MODEL) || checkPropContain(PROP_COMMON_BUILD_DISPLAY_ID, "Flyme");
     }
 
     private static boolean checkProp(String prop) {
         String value = getProperty(prop);
-        return value != null && !Objects.equals(value, UNKNOWN) && !value.isEmpty();
+        return value != null && !Objects.equals(value, UNKNOWN);
+    }
+
+    private static boolean checkPropExist(String prop) {
+        return checkProp(prop) && !getProperty(prop).isEmpty();
+    }
+
+    private static boolean checkPropContain(String prop, String containString) {
+        return checkProp(prop) && getProperty(prop).contains(containString);
     }
 }
