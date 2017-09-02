afa1fbf76638ed201a7f523e9296a8ee53b6186a
Omico <294958213@qq.com>
2017-09-03 00:32:33 +0800

    MainActivity: No longer support statusbar color less than api 21

--- library/src/main/java/me/omico/util/StatusBarUtils.java
+++ library/src/main/java/me/omico/util/StatusBarUtils.java
@@ -7,8 +7,6 @@ import android.content.Context;
 import android.os.Build;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 
 import java.lang.reflect.Method;
@@ -48,24 +46,5 @@ public class StatusBarUtils {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void setStatusBarColor(Activity activity, @ColorRes int statusBarColor) {
         setStatusBarColor(activity.getWindow(), statusBarColor);
-    }
-
-    public static void addStatusBarView(Activity activity, @ColorRes int statusBarColor) {
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) return;
-        View statusBarView = new View(activity);
-        statusBarView.setBackgroundColor(ContextCompat.getColor(activity, statusBarColor));
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                getStatusBarHeight(activity));
-        ((ViewGroup) activity.findViewById(android.R.id.content)).addView(statusBarView, layoutParams);
-    }
-
-    public static int getStatusBarHeight(Context context) {
-        int result = 0;
-        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
     }
 }
