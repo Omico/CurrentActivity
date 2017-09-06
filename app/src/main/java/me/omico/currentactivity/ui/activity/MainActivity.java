6cc2c52a599a36f7b74672949b3e06c459be32c7
Omico <294958213@qq.com>
2017-09-06 23:35:07 +0800

    Code refactoring: MainFragment -> SettingsFragment
    Use android.support.v7.preference.PreferenceFragmentCompat.

--- app/src/main/java/me/omico/currentactivity/ui/activity/MainActivity.java
+++ app/src/main/java/me/omico/currentactivity/ui/activity/MainActivity.java
@@ -2,11 +2,12 @@ package me.omico.currentactivity.ui.activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import me.omico.currentactivity.R;
-import me.omico.currentactivity.ui.fragment.MainFragment;
+import me.omico.currentactivity.ui.fragment.SettingsFragment;
 import me.omico.util.StatusBarUtils;
 
 /**
@@ -20,7 +21,16 @@ public final class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getFragmentManager().beginTransaction().replace(R.id.main_fragment, new MainFragment()).commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(
+                        R.id.main_fragment,
+                        Fragment.instantiate(
+                                MainActivity.this,
+                                SettingsFragment.class.getCanonicalName()
+                        )
+                )
+                .commit();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             StatusBarUtils.setStatusBarColor(this, R.color.colorPrimary);
