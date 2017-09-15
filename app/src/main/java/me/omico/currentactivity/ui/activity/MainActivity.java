065c6158d2c700572f3da4d98955e2b0e9fe369a
Omico <294958213@qq.com>
2017-09-15 21:45:21 +0800

    Use AppCompatBaseActivity
    Former-commit-id: 37a2a43ea212c78ac2bb3a5e6efd90cb7a265622

--- app/src/main/java/me/omico/currentactivity/ui/activity/MainActivity.java
+++ app/src/main/java/me/omico/currentactivity/ui/activity/MainActivity.java
@@ -1,38 +1,43 @@
 package me.omico.currentactivity.ui.activity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
+import me.omico.base.activity.AppCompatBaseActivity;
 import me.omico.currentactivity.R;
 import me.omico.currentactivity.ui.fragment.SettingsFragment;
-import me.omico.util.StatusBarUtils;
 
 /**
  * @author Omico
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(
-                        R.id.main_fragment,
-                        Fragment.instantiate(
-                                MainActivity.this,
-                                SettingsFragment.class.getCanonicalName()
-                        )
-                )
-                .commit();
+        setStatusBarColor(R.color.colorPrimary);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            StatusBarUtils.setStatusBarColor(this, R.color.colorPrimary);
+        replaceSupportFragment(R.id.main_fragment, MainActivity.this,
+                SettingsFragment.class.getCanonicalName());
+    }
+
+    @Override
+    protected int setContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected int setSupportActionBar() {
+        return R.id.toolbar;
+    }
+
+    @Override
+    protected boolean setDisplayHomeAsUpEnabled() {
+        return false;
+    }
+
+    @Override
+    protected CharSequence setTitle() {
+        return getString(R.string.app_name);
     }
 }
---
+++ library/src/main/java/me/omico/base/activity/AppCompatBaseActivity.java
@@ -0,0 +1,60 @@
+package me.omico.base.activity;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import me.omico.util.StatusBarUtils;
+
+/**
+ * @author Omico 2017/9/15
+ */
+
+public abstract class AppCompatBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.setContentView());
+
+        setSupportActionBar((Toolbar) findViewById(this.setSupportActionBar()));
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(this.setDisplayHomeAsUpEnabled());
+            actionBar.setTitle(this.setTitle());
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void setStatusBarColor(@ColorRes int statusBarColor) {
+        StatusBarUtils.setStatusBarColor(this, statusBarColor);
+    }
+
+    protected void replaceSupportFragment(@IdRes int fragmentContainer, Context context, @NonNull String fragmentName) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(fragmentContainer, Fragment.instantiate(context, fragmentName))
+                .commit();
+    }
+
+    @LayoutRes
+    protected abstract int setContentView();
+
+    @IdRes
+    protected abstract int setSupportActionBar();
+
+    protected abstract boolean setDisplayHomeAsUpEnabled();
+
+    protected abstract CharSequence setTitle();
+}
