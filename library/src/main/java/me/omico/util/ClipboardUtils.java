2d2f684d2182c25aa9e19fb8c41a8b7cb2859669
Omico <294958213@qq.com>
2017-10-09 21:42:07 +0800

    ClipboardUtils: Update
    Former-commit-id: b7a118e4e079d611b924ab82772803738113560a

--- library/src/main/java/me/omico/util/ClipboardUtils.java
+++ library/src/main/java/me/omico/util/ClipboardUtils.java
@@ -3,6 +3,7 @@ package me.omico.util;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
@@ -14,35 +15,34 @@ import me.omico.R;
 
 public class ClipboardUtils {
 
-    public static void copyToClipboard(Context context, @StringRes int resId) {
+    public static void copyToClipboard(@NonNull Context context, @StringRes int resId) {
         copyToClipboard(context, context.getPackageName(), context.getString(resId));
     }
 
-    public static void copyToClipboard(Context context, String text) {
+    public static void copyToClipboard(@NonNull Context context, String text) {
         copyToClipboard(context, context.getPackageName(), text);
     }
 
-
-    public static void copyToClipboard(Context context, @StringRes int resId, String text) {
+    public static void copyToClipboard(@NonNull Context context, @StringRes int resId, String text) {
         copyToClipboard(context, context.getString(resId), text);
     }
 
-    public static void copyToClipboard(Context context, String name, @StringRes int resId) {
+    public static void copyToClipboard(@NonNull Context context, String name, @StringRes int resId) {
         copyToClipboard(context, name, context.getString(resId));
     }
 
-    public static void copyToClipboard(Context context, @StringRes int resIdName, @StringRes int resIdText) {
+    public static void copyToClipboard(@NonNull Context context, @StringRes int resIdName, @StringRes int resIdText) {
         copyToClipboard(context, context.getString(resIdName), context.getString(resIdText));
     }
 
-    public static void copyToClipboard(Context context, String name, String text) {
+    public static void copyToClipboard(@NonNull Context context, String name, String text) {
         try {
             ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
             if (clipboardManager != null)
                 clipboardManager.setPrimaryClip(ClipData.newPlainText(name, text));
-            Toast.makeText(context, R.string.copy_success, Toast.LENGTH_LONG).show();
+            Toast.makeText(context, R.string.copy_success, Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            Toast.makeText(context, R.string.copy_failed, Toast.LENGTH_LONG).show();
+            Toast.makeText(context, R.string.copy_failed, Toast.LENGTH_SHORT).show();
         }
     }
 }
