cf6ecfa7e98f609a6f4787992bb4aa1140900abd
Omico <me@omico.icu>
2024-01-17 21:32:44 -0800

    Upload artifact via GitHub Actions

--- ca/app/build.gradle.kts
+++ ca/app/build.gradle.kts
@@ -1,14 +1,26 @@
+import com.android.builder.signing.DefaultSigningConfig
+
 plugins {
     id("ca.android.application")
 }
 
 android {
+    signingConfigs {
+        named("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = DefaultSigningConfig.DEFAULT_PASSWORD
+            keyAlias = DefaultSigningConfig.DEFAULT_ALIAS
+            keyPassword = DefaultSigningConfig.DEFAULT_PASSWORD
+        }
+    }
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
         }
         release {
+            signingConfig = signingConfigs["debug"]
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
